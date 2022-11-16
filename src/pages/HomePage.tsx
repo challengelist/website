dd62fedae15c3b1e32b64dda7279037ad0c6f977
naoei <altenyt@gmail.com>
2022-11-16 16:13:29 -0500

    Add main hero section for homepage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -6,106 +6,15 @@ export function HomePage() {
             <video src="/src/assets/videos/funny.mp4" autoplay={true} loop={true} muted={true}></video>
         </div>
         <div class="home--contents">
-            hi lol<br/>
-            hi lol<br/>
-            hi lol<br/>
-            hi lol<br/>
-            hi lol<br/>
-            hi lol<br/>
-            hi lol<br/>
-            hi lol<br/>
-            hi lol<br/>
-            hi lol<br/>
-            hi lol<br/>
-            hi lol<br/>
-            hi lol<br/>
-            hi lol<br/>
-            hi lol<br/>
-            hi lol<br/>
-            hi lol<br/>
-            hi lol<br/>
-            hi lol<br/>
-            hi lol<br/>
-            hi lol<br/>
-            hi lol<br/>
-            hi lol<br/>
-            hi lol<br/>
-            hi lol<br/>
-            hi lol<br/>
-            hi lol<br/>
-            hi lol<br/>
-            hi lol<br/>
-            hi lol<br/>
-            hi lol<br/>
-            hi lol<br/>
-            hi lol<br/>
-            hi lol<br/>
-            hi lol<br/>
-            hi lol<br/>
-            hi lol<br/>
-            hi lol<br/>
-            hi lol<br/>
-            hi lol<br/>
-            hi lol<br/>
-            hi lol<br/>
-            hi lol<br/>
-            hi lol<br/>
-            hi lol<br/>
-            hi lol<br/>
-            hi lol<br/>
-            hi lol<br/>
-            hi lol<br/>
-            hi lol<br/>
-            hi lol<br/>
-            hi lol<br/>
-            hi lol<br/>
-            hi lol<br/>
-            hi lol<br/>
-            hi lol<br/>
-            hi lol<br/>
-            hi lol<br/>
-            hi lol<br/>
-            hi lol<br/>
-            hi lol<br/>
-            hi lol<br/>
-            hi lol<br/>
-            hi lol<br/>
-            hi lol<br/>
-            hi lol<br/>
-            hi lol<br/>
-            hi lol<br/>
-            hi lol<br/>
-            hi lol<br/>
-            hi lol<br/>
-            hi lol<br/>
-            hi lol<br/>
-            hi lol<br/>
-            hi lol<br/>
-            hi lol<br/>
-            hi lol<br/>
-            hi lol<br/>
-            hi lol<br/>
-            hi lol<br/>
-            hi lol<br/>
-            hi lol<br/>
-            hi lol<br/>
-            hi lol<br/>
-            hi lol<br/>
-            hi lol<br/>
-            hi lol<br/>
-            hi lol<br/>
-            hi lol<br/>
-            hi lol<br/>
-            hi lol<br/>
-            hi lol<br/>
-            hi lol<br/>
-            hi lol<br/>
-            hi lol<br/>
-            hi lol<br/>
-            hi lol<br/>
-            hi lol<br/>
-            hi lol<br/>
-            hi lol<br/>
+            <div class="home--contents-hero">
+                <div class="home--contents-hero-title">
+                    <div class="home--contents-hero-title-welcome">Welcome to</div>
+                    <div class="home--contents-hero-title-name">The Challenge List</div>
+                </div>
+                <div class="home--contents-hero-description">
+                    Welcome to the website of the Geometry Dash Challenge List! Here you'll find the list of the hardest challenges that Geometry Dash's player-base has to offer.
+                </div>
+            </div>
         </div>
     </div>;
 }
