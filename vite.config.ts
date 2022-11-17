6fa140684d50a7fc89664624e099ae8e587bd346
irisu01 <nekomarisakirisame@gmail.com>
2022-11-17 15:09:31 -0500

    add env file support

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
+import { config } from 'dotenv';
 import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
 
+// Import .env variables.
+config();
+
 export default defineConfig({
+  envPrefix: "CL_",
   plugins: [solidPlugin()],
   server: {
     port: 3000,
