1046d0f280fcec9ad3850d05c4893455e37447a0
naoei <altenyt@gmail.com>
2022-11-18 14:27:33 -0500

    Add staffs section

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,21 +1,76 @@
 import anime from "animejs";
-import { createEffect, createSignal } from "solid-js";
+import { createEffect, createSignal, For } from "solid-js";
 import "../styles/pages/home.scss";
+import { InlineAccount } from "../components/InlineAccount";
+import { APIAccount } from "../structures/APIAccount";
 import { Util } from "../util/Util";
 
 export function HomePage() {
     return <div class="home">
-        <div class="home--video">
-            <VideoBackground />
-        </div>
+        <div class="home--video"><VideoBackground/></div>
         <div class="home--contents">
-            <div class="home--contents-hero">
-                <div class="home--contents-hero-title">
-                    <div class="home--contents-hero-title-welcome">Welcome to</div>
-                    <div class="home--contents-hero-title-name">The Challenge List</div>
+            <Hero/>
+            <Staffs/>
+        </div>
+    </div>;
+}
+
+function Hero() {
+    return <div class="home--contents-hero">
+        <div class="home--contents-hero-title">
+            <div class="home--contents-hero-title-welcome">Welcome to</div>
+            <div class="home--contents-hero-title-name">The Challenge List</div>
+        </div>
+        <div class="home--contents-hero-description">
+            Welcome to the website of the Geometry Dash Challenge List! Here you'll find the list of the hardest
+            challenges that Geometry Dash's player-base has to offer.
+        </div>
+    </div>;
+}
+
+function Staffs() {
+    import("../styles/pages/home/staffs.scss");
+
+    const [list, setList] = createSignal<APIAccount[]>([]);
+    const [devs, setDevs] = createSignal<APIAccount[]>([]);
+
+    fetch(`${import.meta.env.CL_API_URL}/api/meta/staff`)
+        .then(response => response.json())
+        .then(res => {
+            let listAccounts = res.data.list as APIAccount[];
+            let devsAccounts = res.data.site as APIAccount[];
+
+            setList(listAccounts);
+            setDevs(devsAccounts);
+        });
+
+    return <div class="home--contents-staff">
+        <div class="home--contents-staff-title">Contacts and Staff Members</div>
+        <div class="home--contents-staff-container">
+            <div class="home--contents-staff-list">
+                <div class="home--contents-staff-list-title">
+                    <div class="home--contents-staff-list-title-title">Challenge List Team</div>
+                    <div class="home--contents-staff-list-title-description">The Challenge List is managed by a fairly sized team of players consisting of the following members.</div>
                 </div>
-                <div class="home--contents-hero-description">
-                    Welcome to the website of the Geometry Dash Challenge List! Here you'll find the list of the hardest challenges that Geometry Dash's player-base has to offer.
+            </div>
+            <div class="home--contents-staff-dev">
+                <div class="home--contents-staff-dev-title">
+                    <div class="home--contents-staff-dev-title-title">Development Team</div>
+                    <div class="home--contents-staff-dev-title-description">This website, and all of its related software and corresponding technologies made for the Challenge List are managed by the following members.</div>
+                </div>
+            </div>
+        </div>
+        <div class="home--contents-staff-container">
+            <div class="home--contents-staff-container-list">
+                <div class="home--contents-staff-list-accounts">
+                    <For each={list()}>{acc =>
+                        <InlineAccount apiAccount={acc} />
+                    }</For>
+                </div>
+                <div class="home--contents-staff-dev-accounts">
+                    <For each={devs()}>{acc =>
+                        <InlineAccount apiAccount={acc}/>
+                    }</For>
                 </div>
             </div>
         </div>
