6d69190b3b1e8326d730f9ce053133f0dcbf7b93
naoei <altenyt@gmail.com>
2022-12-17 09:21:24 -0500

    remove PasswordField and merge into TextField

--- src/components/fields/TextField.tsx
+++ src/components/fields/TextField.tsx
@@ -5,6 +5,7 @@ import "../../styles/components/fields/textField.scss";
 export interface TextFieldProperties {
     onInput?: JSX.EventHandlerUnion<HTMLInputElement, InputEvent>
     name: string;
+    type: "text" | "password";
     placeholder?: string;
     lightness?: number;
     min?: number;
@@ -13,6 +14,6 @@ export interface TextFieldProperties {
 
 export function TextField(props: TextFieldProperties) {
     return <LabelledField name={props.name}>
-        <input class="_text-field" style={`background: var(--hsl-dark${props.lightness ?? 15})`} type="text" {...props} />
+        <input class="_text-field" style={`background: var(--hsl-dark${props.lightness ?? 15})`} {...props} />
     </LabelledField>;
 }
